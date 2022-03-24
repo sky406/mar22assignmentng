df6e8c213382981060cf2d2107322e4083962268
sky406 <60670435+sky406@users.noreply.github.com>
2022-03-24 10:28:02 -0700

    added a router outlet  to the app conponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopnavComponent } from './topnav/topnav.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavComponent
+    TopnavComponent,
+    ProductsComponent,
+    ContactsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
